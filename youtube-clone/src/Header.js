e9354cd50247a2f678ecbe0074f8a7ad1b467ed7
kSabchev <kaloyansabchev@gmail.com>
2020-08-04 22:09:41 +0300

    working on header
    imported MenuSharpIcon anad SearchSharpIcon from materua-ui

--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import MenuSharpIcon from '@material-ui/icons/MenuSharp';
+import SearchSharpIcon from '@material-ui/icons/SearchSharp';
 
 function Header() {
     return (
         <div className="header">
             <h1>I am a header</h1>
+            <MenuSharpIcon />
+            <img 
+            className="header__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+            alt="" />
+            <input type="text"/>
+            <SearchSharpIcon />
         </div>
     )
 }
