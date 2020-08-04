9bb2f1ef6827203c6146fec862f3938514fb8635
kSabchev <kaloyansabchev@gmail.com>
2020-08-04 21:57:32 +0300

    create-react-app
    created app
    removed tests + logo file
    created Header component
    npm install @material-ui/core
    npm install @material-ui/icons

---
+++ youtube-clone/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header"
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Test</h1>
+      <Header></Header>
+
+    {/* Header */}
+    {/* Sidebar */}
+    {/* RecomendedVideoes */}
+    </div>
+  );
+}
+
+export default App;
