e775e099b2dc7269a439b00111f0e93a58e909a6
kSabchev <kaloyansabchev@gmail.com>
2020-08-06 01:49:53 +0300

    Search bar uses useState, using router to link

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import './App.css';
-import Header from "./Header"
-import Sidebar from './Sidebar';
-import RecommendedVideos from './RecommendedVideos';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import RecommendedVideos from "./RecommendedVideos";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      
-      <Header></Header>
-      <div className="app__page">
-        <Sidebar />
-        <RecommendedVideos />
-      </div>
+      <Router>
+        <Header />
 
-    {/* Header */}
-    {/* Sidebar */}
-    {/* RecomendedVideoes */}
+        <Switch>
+          <Route path="/search/:searchTerm">
+            <h1>Search page</h1>
+          </Route>
+          <Route path="/">
+            <div className="app__page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+
+      {/* Header */}
+      {/* Sidebar */}
+      {/* RecomendedVideoes */}
     </div>
   );
 }
--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuSharpIcon from "@material-ui/icons/MenuSharp";
 import SearchSharpIcon from "@material-ui/icons/SearchSharp";
 import VideoCallSharpIcon from "@material-ui/icons/VideoCallSharp";
@@ -6,27 +6,35 @@ import AppsSharpIcon from "@material-ui/icons/AppsSharp";
 import NotificationsSharpIcon from "@material-ui/icons/NotificationsSharp";
 import Avatar from "@material-ui/core/Avatar";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
+  const [inputSearch, setInputSeach] = useState("");
+
   return (
     <div className="header">
       <div className="header__left">
         <MenuSharpIcon />
+        <Link to="/">
         <img
           className="header__logo"
           src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
           alt=""
         />
+        </Link>
       </div>
       <div className="header__input">
-        <input placeholder="Search" type="text" />
+        <input 
+        onChange={e => setInputSeach(e.target.value)} value={inputSearch} placeholder="Search" type="text" />
+        <Link to={`/search/${inputSearch}`}>
         <SearchSharpIcon className="header__inputButton" />
+        </Link>
       </div>
 
       <div className="header__icons">
-        <VideoCallSharpIcon className="header__icon"/>
-        <AppsSharpIcon className="header__icon"/>
-        <NotificationsSharpIcon className="header__icon"/>
+        <VideoCallSharpIcon className="header__icon" />
+        <AppsSharpIcon className="header__icon" />
+        <NotificationsSharpIcon className="header__icon" />
         <Avatar alt="" src="" />
       </div>
     </div>
